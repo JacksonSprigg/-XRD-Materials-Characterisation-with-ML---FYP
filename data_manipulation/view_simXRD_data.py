a238b85042025254d2c8fbf4246fc3bd1833e6df
Jackson Sprigg <jacksonsprigg@gmail.com>
2024-07-26 17:26:00 +1000

    new crystal system

--- data_manipulation/view_simXRD_data.py
+++ data_manipulation/view_simXRD_data.py
@@ -30,6 +30,15 @@ from ase.db import connect
 # simulation_param: Yes - I don't know what this actually means
 # symbols: A list, e.g., ['H', 'H', 'H', 'H', 'C', 'C', 'O', 'O', 'O', 'O', 'O', 'O']
 # tager: [Space Group, Crystal System, ??something else??] - I think the something else is bravis lattice types?
+
+# Note the crystal system meaning:
+#  1 : Cubic
+#  2 : Hexagonal
+#  3 : Tetragonal
+#  4 : Orthorhombic
+#  5 : Trigonal
+#  6 : Monoclinic
+#  7 : Triclinic
 #####################################################################################
 
 ## Note that you may need to change this path depending on permissions ##
@@ -50,6 +59,17 @@ def plot_xrd_data(latt_dis, intensity, chem_form, atomic_mass, spg, crysystem, b
     plt.ylabel('Intensity')
     plt.title(f'X-ray Diffraction Pattern for {(chem_form)}')
     plt.grid(True)
+
+    # Get the name of the crystal system
+    crystal_systems = {
+    1: "Cubic",
+    2: "Hexagonal",
+    3: "Tetragonal",
+    4: "Orthorhombic",
+    5: "Trigonal",
+    6: "Monoclinic",
+    7: "Triclinic"}
+    crystal_system_name = crystal_systems.get(crysystem, "Unknown")
     
     # Data information
     plt.text(0.95, 0.95, f'Formula: {chem_form}', transform=plt.gca().transAxes, 
@@ -58,7 +78,7 @@ def plot_xrd_data(latt_dis, intensity, chem_form, atomic_mass, spg, crysystem, b
             verticalalignment='top', horizontalalignment='right')
     plt.text(0.95, 0.75, f'Space Group: {spg}', transform=plt.gca().transAxes, 
              verticalalignment='top', horizontalalignment='right')
-    plt.text(0.95, 0.70, f'Crystal System: {crysystem}', transform=plt.gca().transAxes, 
+    plt.text(0.95, 0.70, f'Crystal System: {crystal_system_name}', transform=plt.gca().transAxes, 
              verticalalignment='top', horizontalalignment='right')
     plt.text(0.95, 0.65, f'Bravis Latt Type: {bravislatt_type}', transform=plt.gca().transAxes, 
              verticalalignment='top', horizontalalignment='right')
@@ -68,12 +88,12 @@ def plot_xrd_data(latt_dis, intensity, chem_form, atomic_mass, spg, crysystem, b
 
     return
 
-# You can print any values if you want to have a look
+# print values if you want a better idea of what they look like
 def looking_at_data(databs, max_iterations, plot=False):
     count = 0
     for row in databs.select():
 
-        # Note, element is a good form for feeding to ML, chem form is good for plotting
+        # Note, element is a good form for feeding to ML, chem_form is good for reading
         element = getattr(row, 'symbols')
 
         latt_dis = eval(getattr(row, 'latt_dis'))
@@ -97,7 +117,7 @@ def looking_at_data(databs, max_iterations, plot=False):
             break
 
     return 
-#ahh
+
 # Generates plots
 limit = 5       # The amount of XRD rows you want to go through
 plot  = True   # This will save the matplots to scratch folder
